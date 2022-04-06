999898b836acd107533d46a25461d93f2faa8df4
gabrielrf2601 <gabrielfreitas2601@gmail.com>
2022-04-05 21:08:57 -0300

    Adição do componente MovieRow

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,17 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-// import './MovieRow.css'
+import './MovieRow.css'
 
 export default function MovieRow ({ title, items }) {
     console.log(items)
     return (
-        <div>
+        <div className="movieRow">
             <h2>{title}</h2>
-            <div className="movieRow-listarea">
-                {items.results.length > 0 && items.results.map((item, key) => (
-                    <img key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-                ))}
+            <div className="movieRow--listarea">
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
